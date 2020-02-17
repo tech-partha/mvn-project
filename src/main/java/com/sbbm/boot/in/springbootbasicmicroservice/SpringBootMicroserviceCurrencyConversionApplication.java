a0ac6a13807e9fdf0373fe7d73bb7a47879fbff6
Partha <samal.parthasarathi987@gmail.com>
2020-02-17 19:36:58 +0000

    ddd

---
+++ src/main/java/com/sbbm/boot/in/springbootbasicmicroservice/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,13 @@
+package com.sbbm.boot.in.springbootbasicmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+	}
+
+}
